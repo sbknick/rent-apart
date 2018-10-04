65a32cadf3b22689b4af557a076ab0464cd5aaaa
Blair <b.knickerbocker@shieldsafety.co.uk>
2018-10-04 15:05:59 +0100

    Added basic messaging models

---
+++ server/src/model/message.model.ts
@@ -0,0 +1,15 @@
+import { User } from "./user.model";
+
+export class Message
+{
+    constructor(private from: User, private content: string)
+    {}
+}
+
+export class ChatMessage extends Message
+{
+    constructor(from: User, content: string)
+    {
+        super(from, content);
+    }
+}
