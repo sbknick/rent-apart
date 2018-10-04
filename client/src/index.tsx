e8e589edba6e435880e31430e4d15bdc371cb359
Blair <b.knickerbocker@shieldsafety.co.uk>
2018-10-04 17:05:22 +0100

    Added hello world react project

---
+++ client/src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { Component } from 'react'
+import { render } from 'react-dom'
+// import './index.css'
+
+import "./index.scss";
+
+class App extends Component 
+{
+  render()
+  {
+    return (<h1>Hello World!</h1>);
+  }
+}
+
+render(<App />, document.getElementById('root'));
