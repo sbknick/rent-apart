4cb463ede62620a6c35ce5ccfd0409d92462f202
Blair <b.knickerbocker@shieldsafety.co.uk>
2018-10-05 14:09:29 +0100

    meh

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
-import { Component } from "react";
 import * as ReactDOM from "react-dom";
 import { hot } from "react-hot-loader";
 import "./index.scss";
 import App from "./ui/App";
 
-declare var module;
+declare var module: any;
 
 let MyApp = hot(module)(App);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MyApp />, document.getElementById('root'));
