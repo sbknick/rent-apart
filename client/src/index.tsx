19b2a968aadf3deaa4f79d550f6696153884d630
Blair <b.knickerbocker@shieldsafety.co.uk>
2018-10-05 10:23:19 +0100

    Added example chat client, it works?

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,16 +3,9 @@ import { Component } from "react";
 import * as ReactDOM from "react-dom";
 import { hot } from "react-hot-loader";
 import "./index.scss";
+import App from "./ui/App";
 
 declare var module;
-
-class App extends Component 
-{
-  render()
-  {
-    return (<h1>Hello World!</h1>);
-  }
-}
 
 let MyApp = hot(module)(App);
 
