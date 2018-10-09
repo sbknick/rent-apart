3542d445718b94f133d7dbb36da5af769937cfa4
Blair <b.knickerbocker@shieldsafety.co.uk>
2018-10-09 13:01:28 +0100

    added map assets

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { hot } from "react-hot-loader";
+
 import "./index.scss";
+
 import App from "./ui/App";
 
 declare var module: any;
